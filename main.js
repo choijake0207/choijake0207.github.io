80ab2d32e4c4e8425b9d7cb48cb0c53145a9941d
choijake0207 <jake.ch0712@gmail.com>
2024-09-30 17:03:29 -0700

    add toggle logic for resume iframe

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const hiddenEl = document.querySelectorAll(".hidden")
     hiddenEl.forEach((el) => observer.observe(el))
 })
+
+const resumeContainer = document.querySelector(".resume-overlay")
+const viewResumeBtn = document.querySelector(".view-resume-btn")
+viewResumeBtn.addEventListener("click", () => {
+    resumeContainer.classList.remove("none")
+    document.body.style.overflowY = "hidden"
+    
+})
+const closeResumeBtn = document.querySelector(".close-resume-btn")
+closeResumeBtn.addEventListener("click", () => {
+    resumeContainer.classList.add("none")
+    document.body.style.overflowY = "scroll"
+})
